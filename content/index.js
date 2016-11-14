736613c417e8e495a5e647a09282b989b45fbc45
Frank Yang <yangpoan@gmail.com>
2016-11-14 21:38:12 +0800

    update banner

--- content/index.js
+++ content/index.js
@@ -1,7 +1,12 @@
 const data = {
   "title": "NetDB - 前瞻網路實驗室",
-  "banner-title": "NetDB",
-  "banner-intro": "INNOVATION · PASSION · SHARING"
+  "header-title": "NETDB",
+  "about-title": "About",
+  "project-title": "Project",
+  "member-title": "Member",
+  "banner-title": "NETDB",
+  // "banner-intro": "PASSION · INNOVATION · SHARING",
+  "banner-intro": "熱情 · 創新 · 分享",
 }
 
 module.exports = data;
