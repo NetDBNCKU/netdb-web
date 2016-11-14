921582912940ba1f33cdae0f4443d4c1e9997e73
FrankYang0529 <frankyang.netdb@icloud.com>
2016-11-14 22:28:33 +0800

    add section setting

--- content/index.js
+++ content/index.js
@@ -7,6 +7,10 @@ const data = {
   "banner-title": "NETDB",
   // "banner-intro": "PASSION · INNOVATION · SHARING",
   "banner-intro": "熱情 · 創新 · 分享",
+  "about-description":"",
+  "about-research-field-1":"Data Analysis",
+  "about-research-field-2":"Data Visualization",
+  "about-research-field-3":"Distrubuted Computing",
 }
 
 module.exports = data;
