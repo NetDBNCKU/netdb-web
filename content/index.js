9f5abeea3d2843335ee989ff8a0ea59650eaab75
peihsuan1225 <peihsuan1225@gmail.com>
2016-11-15 00:55:11 +0800

    about layout update

--- content/index.js
+++ content/index.js
@@ -7,10 +7,13 @@ const data = {
   "banner-title": "NETDB",
   // "banner-intro": "PASSION · INNOVATION · SHARING",
   "banner-intro": "熱情 · 創新 · 分享",
-  "about-description":"",
+  "about-description":"我們因為有共同的理念(熱情、創新、分享)而聚集。我們因為有共同的理念(熱情、創新、分享)而聚集。我們因為有共同的理念(熱情、創新、分享)而聚集。我們因為有共同的理念(熱情、創新、分享)而聚集。我們因為有共同的理念(熱情、創新、分享)而聚集。我們因為有共同的理念(熱情、創新、分享)而聚集。",
   "about-research-field-1":"Data Analysis",
+  "about-research-field-alt-1":"Data-Analysis",
   "about-research-field-2":"Data Visualization",
+  "about-research-field-alt-2":"Data-Visualization",
   "about-research-field-3":"Distrubuted Computing",
+  "about-research-field-alt-3":"Distrubuted-Computing",
 }
 
 module.exports = data;
