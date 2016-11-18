6fb4d3f61c9f90f2fd49af06fe15c0bfe5a36192
Frank Yang <yangpoan@gmail.com>
2016-11-18 16:33:52 +0800

    add header scroll css

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
-(function(){
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+// smooth scroll
+$('a[href*="#"]:not([href="#"])').click(function() {
+  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
 
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
+    if (target.length) {
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000);
+      return false;
     }
-  });
-})();
+  }
+});
+
+// header css
+if($('body').scrollTop() > $('#banner').height()) {
+  $('#header').addClass("header-scroll");
+} else {
+  $('#header').removeClass("header-scroll");
+}
+
+$(window).on("scroll", function(e) {
+  if($('body').scrollTop() > $('#banner').height()) {
+    $('#header').addClass("header-scroll");
+  } else {
+    $('#header').removeClass("header-scroll");
+  }
+});
