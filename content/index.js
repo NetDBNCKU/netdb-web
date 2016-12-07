3332435926aa0ee3f6478abe01c9c0195c15ffa8
peihsuan1225 <peihsuan1225@gmail.com>
2016-12-07 22:38:07 +0800

    add member section layout

--- content/index.js
+++ content/index.js
@@ -9,11 +9,37 @@ const data = {
   "banner-intro": "熱情 · 創新 · 分享",
   "about-description":"我們因為有共同的理念(熱情、創新、分享)而聚集。我們因為有共同的理念(熱情、創新、分享)而聚集。我們因為有共同的理念(熱情、創新、分享)而聚集。我們因為有共同的理念(熱情、創新、分享)而聚集。我們因為有共同的理念(熱情、創新、分享)而聚集。我們因為有共同的理念(熱情、創新、分享)而聚集。",
   "about-research-field-1":"Data Analysis",
+  "about-research-field-img-1":"image/heart.png",
   "about-research-field-alt-1":"Data-Analysis",
   "about-research-field-2":"Data Visualization",
+  "about-research-field-img-2":"image/heart.png",
   "about-research-field-alt-2":"Data-Visualization",
   "about-research-field-3":"Distrubuted Computing",
+  "about-research-field-img-3":"image/heart.png",
   "about-research-field-alt-3":"Distrubuted-Computing",
+  "project-item-img-1":"image/heart.png",
+  "project-item-alt-1":"themosquitoman",
+  "project-item-1":"themosquitoman",
+  "project-item-description-1":"掌蚊人是跨領域專案",
+  "project-item-img-2":"image/heart.png",
+  "project-item-alt-2":"project2",
+  "project-item-2":"project2",
+  "project-item-description-2":"project2",
+  "project-item-img-3":"image/heart.png",
+  "project-item-alt-3":"project3",
+  "project-item-3":"project3",
+  "project-item-description-3":"project3",
+  "current-member-title":"Current Member",
+  "current-member-list":[
+    { Name: "Kun-Ta Chuang (prof.)", Specialty: "Data Analysis",imageURL:"image/member/ktchuang.png"},
+    { Name: "Kun-Ta Chuang (prof.)", Specialty: "Data Analysis",imageURL:"image/member/ktchuang.png"},
+    { Name: "Kun-Ta Chuang (prof.)", Specialty: "Data Analysis",imageURL:"image/member/ktchuang.png"}
+  ],
+  "alumni-member-title":"Alumni",
+  "alumni-list":[
+    { Name: "Donut man", Specialty: "eat"}
+  ]
+
 }
 
 module.exports = data;
