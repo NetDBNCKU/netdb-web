1ea861cfeeddcf395793dd4d19a2d727d6cfd4f4
LeeW <cl87654321@gmail.com>
2016-12-18 14:57:00 +0800

    Simply list all projects without design

---
+++ content/proj-data.js
@@ -0,0 +1,189 @@
+const data = {
+  "proj-list": [
+    {
+      "fields": [
+        "Data Mining",
+        "Patterns Mining",
+        "Spatial and Temporal Analysis",
+        "Data Visualization",
+        "Text Mining"
+      ],
+      "image_url": "",
+      "project": "掌蚊人"
+    },
+    {
+      "fields": [
+        "Data Mining",
+        "Spatial and Temporal Analysis",
+        "Data Visualization",
+        "Open Data"
+      ],
+      "image_url": "",
+      "project": "用數據看台灣"
+    },
+    {
+      "fields": [
+        "Open Data"
+      ],
+      "image_url": "",
+      "project": "Instants"
+    },
+    {
+      "fields": [
+        "Data Visualization"
+      ],
+      "image_url": "",
+      "project": "My health bank"
+    },
+    {
+      "fields": [
+        "Data Visualization"
+      ],
+      "image_url": "",
+      "project": "即時問卷系統"
+    },
+    {
+      "fields": [
+        "Data Mining",
+        "Spatial and Temporal Analysis"
+      ],
+      "image_url": "",
+      "project": "LASS"
+    },
+    {
+      "fields": [
+        "Distributed System"
+      ],
+      "image_url": "",
+      "project": "III 2013 (軌跡分析by Hadoop)"
+    },
+    {
+      "fields": [
+        "Distributed System"
+      ],
+      "image_url": "",
+      "project": "III 2014 （Foursquare on Hadoop and BigSQL）"
+    },
+    {
+      "fields": [
+        "Distributed System"
+      ],
+      "image_url": "",
+      "project": "III 2015 (Mahout FPM and Apache Pig)"
+    },
+    {
+      "fields": [
+        "Data Mining",
+        "Text Mining",
+        "Graph Mining"
+      ],
+      "image_url": "",
+      "project": "III 2016 (文字雲 by 新聞資料 and DBpedia Graph)"
+    },
+    {
+      "fields": [
+        "Open Data"
+      ],
+      "image_url": "",
+      "project": "OpenPG (Web Open Data FDW)"
+    },
+    {
+      "fields": [
+        "Distributed System"
+      ],
+      "image_url": "",
+      "project": "AFPM (分散式FPM快速估計演算法)"
+    },
+    {
+      "fields": [
+        "Data Mining",
+        "Graph Mining"
+      ],
+      "image_url": "",
+      "project": "Find Right Things from Knowledge Graph"
+    },
+    {
+      "fields": [
+        "Data Mining",
+        "Spatial and Temporal Analysis"
+      ],
+      "image_url": "",
+      "project": "Dengue ST index"
+    },
+    {
+      "fields": [
+        "Data Storage"
+      ],
+      "image_url": "",
+      "project": "HPT(VoltDB 高效交易運算)"
+    },
+    {
+      "fields": [
+        "Data Storage"
+      ],
+      "image_url": "",
+      "project": "iServDB"
+    },
+    {
+      "fields": [
+        "Data Storage"
+      ],
+      "image_url": "",
+      "project": "iServStor"
+    },
+    {
+      "fields": [
+        "Data Storage"
+      ],
+      "image_url": "",
+      "project": "異質性資料庫整合:中小企業雲端服務中介平台與閘道器技術研發"
+    },
+    {
+      "fields": [
+        "Data Mining",
+        "Patterns Mining"
+      ],
+      "image_url": "",
+      "project": "智慧安全家庭守護平台開發計畫"
+    },
+    {
+      "fields": [
+        "Data Visualization",
+        "Distributed System"
+      ],
+      "image_url": "",
+      "project": "建立於Spark平台上之異質串流資料分析及視覺化環境"
+    },
+    {
+      "fields": [
+        "Application"
+      ],
+      "image_url": "",
+      "project": "cssiot 2016 (Gomajii crawler)"
+    },
+    {
+      "fields": [
+        "Application"
+      ],
+      "image_url": "",
+      "project": "抽獎機"
+    },
+    {
+      "fields": [
+        "Data Mining",
+        "Graph Mining"
+      ],
+      "image_url": "",
+      "project": "Knowledge Enrichment"
+    },
+    {
+      "fields": [
+        "Application"
+      ],
+      "image_url": "",
+      "project": "Matlab輔助運算App (PhoneGap, Python串接Matlab)"
+    }
+  ]
+}
+
+module.exports = data;
